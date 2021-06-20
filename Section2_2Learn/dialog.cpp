bda4c224609d0125f356bd50275bb399b52204f9
MegatronChen <kongfufatcat@163.com>
2021-06-20 20:50:11 +0800

    Section 2.3 Done on Dell

--- Section2_2Learn/dialog.cpp
+++ Section2_2Learn/dialog.cpp
@@ -6,6 +6,9 @@ Dialog::Dialog(QWidget *parent)
     , ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    connect(ui->rBtnBlue,SIGNAL(clicked()),this,SLOT(setTextFontColor()));
+    connect(ui->rBtnRed,SIGNAL(clicked()),this,SLOT(setTextFontColor()));
+    connect(ui->rBtnBlack,SIGNAL(clicked()),this,SLOT(setTextFontColor()));
 }
 
 Dialog::~Dialog()
@@ -13,3 +16,38 @@ Dialog::~Dialog()
     delete ui;
 }
 
+
+void Dialog::on_checkBoxUnder_clicked(bool checked)
+{
+    QFont font=ui->txtEdit->font();
+    font.setUnderline(checked);
+    ui->txtEdit->setFont(font);
+}
+
+void Dialog::on_checkBoxItalic_clicked(bool checked)
+{
+    QFont font=ui->txtEdit->font();
+    font.setItalic(checked);
+    ui->txtEdit->setFont(font);
+}
+
+void Dialog::on_checkBoxBold_clicked(bool checked)
+{
+    QFont font=ui->txtEdit->font();
+    font.setBold(checked);
+    ui->txtEdit->setFont(font);
+}
+
+void Dialog::setTextFontColor()
+{
+    QPalette plet=ui->txtEdit->palette();
+    if (ui->rBtnBlue->isChecked())
+        plet.setColor(QPalette::Text,Qt::blue);
+    else if (ui->rBtnRed->isChecked())
+        plet.setColor(QPalette::Text,Qt::red);
+    else if (ui->rBtnBlack->isChecked())
+        plet.setColor(QPalette::Text,Qt::black);
+    else
+        plet.setColor(QPalette::Text,Qt::black);
+    ui->txtEdit->setPalette(plet);
+}
